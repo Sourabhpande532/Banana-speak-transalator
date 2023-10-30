db0a5c6dacc63ccd948f2dd72878f42698e52b8a
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-10-30 16:02:19 +0530

    practice - prompt & alert in vanilla js

---
+++ index.js
@@ -0,0 +1,6 @@
+const userInput = prompt("What's your Name ?");
+alert("Name is " + userInput);
+const userButton = document.querySelector(".btn");
+userButton.addEventListener("click", ()=>{
+    alert("user is clicking")
+}) 
